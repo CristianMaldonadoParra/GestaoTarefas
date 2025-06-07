3e3fe891a58a50c45861b9e1154aabef65dee8f9
Cristian Parra <cristian.maldonado.parra@hotmail.com>
2025-06-07 11:28:41 -0300

    feat(comentario): Os comentários devem ser registrados no histórico de alterações da tarefa.

--- Domain/Services/Comentarios/ComentarioService.cs
+++ Domain/Services/Comentarios/ComentarioService.cs
@@ -1,4 +1,6 @@
 using Application.Dto.Dtos.Comentarios;
+using Domain.Constant;
+using Domain.Entities;
 using Domain.Interfaces.Repository;
 using FluentValidation;
 
@@ -6,9 +8,41 @@ namespace Domain.Services.Comentarios
 {
     public class ComentarioService : ComentarioServiceBase
     {
-        public ComentarioService(IComentarioRepository comentarioRepository, IValidator<ComentarioDto> validator)
+        private readonly IHistoricoAtualizacaoRepository _historicoAtualizacaoRepository;
+        public ComentarioService(IComentarioRepository comentarioRepository, IValidator<ComentarioDto> validator, IHistoricoAtualizacaoRepository historicoAtualizacaoRepository)
             : base(comentarioRepository, validator)
         {
+            _historicoAtualizacaoRepository = historicoAtualizacaoRepository;
+        }
+
+        public override async Task<ComentarioResult> Create(ComentarioDto dto)
+        {
+
+            var result = await base.Create(dto);
+
+            if (result.ValidationResult.IsValid)
+            {
+                var tarefaId = result.Comentario.TarefaId;
+                await AdicionarSeAlterado(tarefaId, "Texto", dto.Texto, result.Comentario.Texto);
+            }
+            return result;
+        }
+
+        private async Task AdicionarSeAlterado(int tarefaId, string campo, object novoValor, object valorAntigo)
+        {
+            if (!Equals(novoValor, valorAntigo))
+            {
+                _historicoAtualizacaoRepository.Add(
+                    new HistoricoAtualizacao(
+                        tarefaId,
+                        UsuarioServiceExternoConst.UserName,
+                        campo,
+                        novoValor?.ToString(),
+                        valorAntigo?.ToString()
+                    )
+                );
+                await _historicoAtualizacaoRepository.CommitAsync();
+            }
         }
     }
 }
